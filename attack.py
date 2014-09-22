41000569dbe565a5cfce92ac2ca0188047249ddf
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2014-09-22 21:39:35 +0200

    continued the attack analysis

--- attack.py
+++ attack.py
@@ -7,35 +7,30 @@ from scipy.io import wavfile
 
 import concept
 
-def run_ana(data, fs, first_sample, length, freq):
-    signal = data[first_sample:first_sample + length]
-    tmp = list(concept.analyse_note(signal, fs, freq, 10))
-    x1 = np.array([i[0] for i in tmp])
-    b1 = np.array([i[1] for i in tmp])
-    p1 = np.array([i[2] for i in tmp])
-    return x1, b1, p1
-
 def run():
     recording = 'data/11-oktave-4.wav'
     first_sample = 5130082
     freq = 526.
     length = 2**13
     
-    result = dict()
-    
     fs, data = wavfile.read(recording)
-    result[first_sample] = run_ana(data, fs, first_sample, length, freq)
 
-    for sample in range(first_sample - 9*fs, first_sample, 2**14):
-        result[sample] = run_ana(data, fs, sample, length, freq)
+    result = dict()
+
+    signal = data[first_sample:first_sample + length]
+    tmp = list(concept.analyse_note(signal, fs, freq, 10))
+    x1 = np.array([i[0] for i in tmp])
+    b1 = np.array([i[1] for i in tmp])
+    p1 = np.array([i[2] for i in tmp])
+    result[first_sample] = p1
+
+    for sample in range(first_sample - 3*fs, first_sample + 2*fs, 2**10):
+        signal = data[sample:sample + length]
+        spec = concept.decibel(concept.windowed_spectrum(signal))
+        result[sample] = spec[b1]
 
     for k, v in sorted(result.items()):
-        x1, b1, p1 = v
-        line = list()
-        for b, p in zip(b1, p1):
-            line.append(b)
-            line.append(p)
-        print(k, *line)
+        print(1.*k/fs, *v)
 
 if __name__ == '__main__':
     run()
