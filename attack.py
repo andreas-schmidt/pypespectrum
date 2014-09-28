63f9f6e1cf810650ef3755024cb893b121e87acc
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2014-09-28 16:43:18 +0200

    extracted steady_state function

--- attack.py
+++ attack.py
@@ -11,20 +11,33 @@ def fftscan(recording, first_sample, freq, length):
     fs, data = wavfile.read(recording)
 
     result = dict()
-
-    signal = data[first_sample:first_sample + length]
-    tmp = list(analyse_note(signal, fs, freq, 10))
-    x1 = np.array([i[0] for i in tmp])
-    b1 = np.array([i[1] for i in tmp])
-    p1 = np.array([i[2] for i in tmp])
-    result[1. * first_sample / fs] = p1
+    peakbins, peakvals = steady_state(data, fs, first_sample, length, freq)
+    result[1. * first_sample / fs] = peakvals
 
     for sample in range(first_sample - 3*fs, first_sample + 2*fs, 2**10):
         signal = data[sample:sample + length]
         spec = decibel(windowed_spectrum(signal))
-        result[1. * sample / fs] = spec[b1]
+        result[1. * sample / fs] = spec[peakbins]
 
     return result
+
+def steady_state(data, fs, reference, length, frequency):
+    """Determine the steady state spectrum.
+    
+    :param data: audio samples
+    :param fs: sampling frequency
+    :param reference: start sample for the steady state spectrum
+    :param length: number of samples to use, should be constant
+                   throughout the analysis and 2**n for fft speed
+    :param frequency: rough estimator of the fundamental frequency
+    :returns: peakbins (fft bin numbers), peakvals (values of the peaks)
+    """
+    signal = data[reference:reference + length]
+    tmp = list(analyse_note(signal, fs, frequency, 10))
+    peakbins = np.array([i[1] for i in tmp])
+    peakvals = np.array([i[2] for i in tmp])
+
+    return peakbins, peakvals
 
 if __name__ == '__main__':
     result = fftscan(
