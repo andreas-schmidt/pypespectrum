f30813800ec3b07fa2af78f05be2c01813292014
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2014-09-28 16:19:31 +0200

    some function parameters

--- attack.py
+++ attack.py
@@ -5,32 +5,37 @@ from __future__ import print_function
 import numpy as np
 from scipy.io import wavfile
 
-import concept
+from concept import analyse_note, decibel, windowed_spectrum
 
-def run():
-    recording = 'data/11-oktave-4.wav'
-    first_sample = 5130082
-    freq = 526.
-    length = 2**13
-    
+def fftscan(recording, first_sample, freq, length):
     fs, data = wavfile.read(recording)
 
     result = dict()
 
     signal = data[first_sample:first_sample + length]
-    tmp = list(concept.analyse_note(signal, fs, freq, 10))
+    tmp = list(analyse_note(signal, fs, freq, 10))
     x1 = np.array([i[0] for i in tmp])
     b1 = np.array([i[1] for i in tmp])
     p1 = np.array([i[2] for i in tmp])
-    result[first_sample] = p1
+    result[1. * first_sample / fs] = p1
 
     for sample in range(first_sample - 3*fs, first_sample + 2*fs, 2**10):
         signal = data[sample:sample + length]
-        spec = concept.decibel(concept.windowed_spectrum(signal))
-        result[sample] = spec[b1]
+        spec = decibel(windowed_spectrum(signal))
+        result[1. * sample / fs] = spec[b1]
 
-    for k, v in sorted(result.items()):
-        print(1.*k/fs, *v)
+    return result
 
 if __name__ == '__main__':
-    run()
+    result = fftscan(
+        recording='data/11-oktave-4.wav',
+        first_sample=5130082,
+        freq=526.,
+        length=2**13
+    )
+
+#   out = open(filename, 'w')
+    for k, v in sorted(result.items()):
+        print(k, *v)#, file=out)
+#   out.close()
+
